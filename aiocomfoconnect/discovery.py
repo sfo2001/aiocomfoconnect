0f2ded386cce0f82e7d92d4f7d7ccfb072ce0813
Stefan Förster <stefan.foerster.in@gmail.com>
2025-06-03 19:32:28 +0200

    Fixed an error in discovery.py that caused the discovery process to fail (no units found in local network, udp packet did not even leave the interface). This was due the default Python broadcast address being used (which is not working in all cases).

--- aiocomfoconnect/discovery.py
+++ aiocomfoconnect/discovery.py
@@ -27,7 +27,9 @@ from __future__ import annotations
 
 import asyncio
 import logging
-from typing import Any, List, Union
+from typing import Any, List, Union, Optional
+
+import netifaces
 
 from .bridge import Bridge
 from .protobuf import zehnder_pb2
@@ -54,7 +56,7 @@ class BridgeDiscoveryProtocol(asyncio.DatagramProtocol):
         _timeout (asyncio.Handle): Handle for the timeout callback.
     """
 
-    def __init__(self, target: str = None, timeout: int = 5):
+    def __init__(self, target: Optional[str] = None, timeout: int = 5):
         loop = asyncio.get_running_loop()
 
         self._bridges: List[Bridge] = []
@@ -77,8 +79,17 @@ class BridgeDiscoveryProtocol(asyncio.DatagramProtocol):
             _LOGGER.debug("Sending discovery request to %s:%d", self._target, Bridge.PORT)
             self.transport.sendto(b"\x0a\x00", (self._target, Bridge.PORT))
         else:
-            _LOGGER.debug("Sending discovery request to broadcast:%d", Bridge.PORT)
-            self.transport.sendto(b"\x0a\x00", ("<broadcast>", Bridge.PORT))
+            # Determine broadcast address programmatically
+            try:
+                gws = netifaces.gateways()
+                default_iface = gws['default'][netifaces.AF_INET][1]
+                addrs = netifaces.ifaddresses(default_iface)
+                broadcast_addr = addrs[netifaces.AF_INET][0].get('broadcast', '255.255.255.255')
+            except Exception as e:
+                _LOGGER.warning("Could not determine broadcast address, using 255.255.255.255: %s", e)
+                broadcast_addr = '255.255.255.255'
+            _LOGGER.debug("Sending discovery request to broadcast:%d (%s)", Bridge.PORT, broadcast_addr)
+            self.transport.sendto(b"\x0a\x00", (broadcast_addr, Bridge.PORT))
 
     def datagram_received(self, data: Union[bytes, str], addr: tuple[str | Any, int]):
         """
@@ -93,12 +104,15 @@ class BridgeDiscoveryProtocol(asyncio.DatagramProtocol):
             return
 
         _LOGGER.debug("Data received from %s: %s", addr, data)
+        try:
+            # Decode the response
+            parser = zehnder_pb2.DiscoveryOperation()  # pylint: disable=no-member
+            parser.ParseFromString(data)
 
-        # Decode the response
-        parser = zehnder_pb2.DiscoveryOperation()  # pylint: disable=no-member
-        parser.ParseFromString(data)
-
-        self._bridges.append(Bridge(host=parser.searchGatewayResponse.ipaddress, uuid=parser.searchGatewayResponse.uuid.hex()))
+            self._bridges.append(Bridge(host=parser.searchGatewayResponse.ipaddress, uuid=parser.searchGatewayResponse.uuid.hex()))
+        except Exception as exc:
+            _LOGGER.error("Failed to parse discovery response from %s: %s", addr, exc)
+            return
 
         # When we have passed a target, we only want to listen for that one
         if self._target:
@@ -123,7 +137,7 @@ class BridgeDiscoveryProtocol(asyncio.DatagramProtocol):
         return self._future
 
 
-async def discover_bridges(host: str = None, timeout: int = 1, loop=None) -> List[Bridge]:
+async def discover_bridges(host: Optional[str] = None, timeout: int = 5, loop=None) -> List[Bridge]:
     """
     Discover ComfoConnect bridges on the local network or at a specified host.
 
